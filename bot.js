0f8e340871592ae686fe56d97c7932fd665291b3
alialmeahy <45081465+alialmeahy@users.noreply.github.com>
2019-02-09 13:52:36 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -349,7 +349,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`44play}مبرمج البوت ali almeahy`,"http://twitch.tv/Death Shop")
+client.user.setGame(`44play}44skip_44stop`,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
